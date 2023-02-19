7bb75262410261d4a367b0ff95835886f4e33331
Lucky-Star-999 <theboost1305@gmail.com>
2023-02-19 16:50:15 +0700

    Make sample layout

--- src/main/java/com/example/foodservicev2/controller/TestController.java
+++ src/main/java/com/example/foodservicev2/controller/TestController.java
@@ -1,15 +1,10 @@
 package com.example.foodservicev2.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/test")
 public class TestController {
     
-    @GetMapping("")
-    public String hello() {
-        return "Hi";
-    }
 }
