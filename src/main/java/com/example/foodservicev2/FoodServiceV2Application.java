25a436cd39e5accfeccb2c3b98cd8af968ab9af9
Lucky-Star-999 <theboost1305@gmail.com>
2023-02-19 09:45:00 +0700

    Init

---
+++ src/main/java/com/example/foodservicev2/FoodServiceV2Application.java
@@ -0,0 +1,13 @@
+package com.example.foodservicev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodServiceV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodServiceV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foodservicev2/FoodServiceV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodservicev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodServiceV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
